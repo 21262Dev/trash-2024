acab39f6f823c13de3b7f46e06a0c1227e8df055
21262 <21262webdev@gmail.com>
2024-03-27 23:10:25 +0530

    better punishmentsss :D:
    commit d03932a2c59fad543a1f7094e4b8afe2623c7735
    Author: 21262 <21262webdev@gmail.com>
    Date:   Wed Mar 27 23:08:45 2024 +0530
    
        Even better punishment
    
    commit 2f3cb816c1bbdf9af2af2a39a22174b75a9d1aa6
    Author: 21262 <21262webdev@gmail.com>
    Date:   Wed Mar 27 23:07:33 2024 +0530
    
        Punishes user every 100 seconds

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 let playerHP = 0;
+let enemyHP = 0
 if (playerHP > 100) {
     console.log("Stop it you Cheater!")
+    
+    enemyHP++;
+    window.setInterval(() => {
+         alert("Better punishment")
+    }, 100);
+
 }
+
